77dbd24bb1c925162dcb2f138e8560cb706ab2ef
mtaruno <mtaruno@uw.edu>
2026-02-05 02:00:32 -0500

    Created catkin_ws structure

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/gixstudent/pyroscope/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/gixstudent/pyroscope/devel;/opt/ros/melodic'.split(';') if '/home/gixstudent/pyroscope/devel;/opt/ros/melodic' != '' else []
