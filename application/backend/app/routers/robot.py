a693ffe0543acaff8156ca6ee6100c1b4d5fb798
mtaruno <mtaruno@uw.edu>
2026-02-16 17:21:18 -0800

    updated to source the base ROS environment

--- application/backend/app/routers/robot.py
+++ application/backend/app/routers/robot.py
@@ -73,6 +73,7 @@ async def start_coverage_mission(config: MissionConfig = None):
         # Build roslaunch command with parameters
         cmd = [
             'bash', '-c',
+            f'source /opt/ros/melodic/setup.bash && '
             f'source ~/pyroscope/catkin_ws/devel/setup.bash && '
             f'roslaunch pyroscope_navigation coverage_mission.launch '
             f'area_width:={config.area_width if config else 5.0} '
