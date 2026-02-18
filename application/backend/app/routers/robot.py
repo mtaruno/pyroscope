e7d13a4957ccd08d39193d1c3e223f187f50e00b
mtaruno <mtaruno@uw.edu>
2026-02-17 21:03:49 -0800

    Update the subprocess commands for env setup

--- application/backend/app/routers/robot.py
+++ application/backend/app/routers/robot.py
@@ -73,7 +73,8 @@ async def start_coverage_mission(config: MissionConfig = None):
         # Build roslaunch command with parameters
         cmd = [
             'bash', '-c',
-            f'unset VIRTUAL_ENV && unset CONDA_DEFAULT_ENV && unset PYTHONPATH && unset PYTHONHOME && '
+            f'unset VIRTUAL_ENV && unset CONDA_DEFAULT_ENV && unset PYTHONHOME && '
+            f'export PYTHONPATH=/usr/lib/python2.7/dist-packages && '
             f'export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin && '
             f'export PYTHON=/usr/bin/python2.7 && '
             f'source /opt/ros/melodic/setup.bash && '
